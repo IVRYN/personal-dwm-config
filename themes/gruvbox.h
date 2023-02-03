23fe133b8816a01faa179052de3c083c69ac1bce
IVRYN <aizofmulia@gmail.com>
2023-02-03 16:40:29 +0800

    Added themes/gruvbox theme

---
+++ themes/gruvbox.h
@@ -0,0 +1,30 @@
+/*
+ *  The theme is based on the gruvbox hard dark
+ */
+
+#ifndef GRUVBOX_H
+#define GRUVBOX_H
+
+static const char background[]  =   "#1d2021";
+static const char foreground[]  =   "#d5c4a1";
+
+// Normal Colours
+static const char nBlack[]      =   "#32302F";
+static const char nRed[]        =   "#cc241d";
+static const char nGreen[]      =   "#98971a";
+static const char nYellow[]     =   "#d79921";
+static const char nBlue[]       =   "#458588";
+static const char nMagenta[]    =   "#b16286";
+static const char nCyan[]       =   "#689d6a";
+static const char nWhite[]      =   "#a89984";
+
+// Bright Colours
+static const char bBlack[]      =   "#504945";
+static const char bRed[]        =   "#fb4934";
+static const char bGreen[]      =   "#b8bb26";
+static const char bYellow[]     =   "#fabd2f";
+static const char bBlue[]       =   "#83a598";
+static const char bMagenta[]    =   "#d38698";
+static const char bCyan[]       =   "#8ec07c";
+static const char bWhite[]      =   "#ebdbb2";
+#endif
