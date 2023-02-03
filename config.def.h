9b4ba7c6d660473fb3aac27be201f9681571242f
IVRYN <aizofmulia@gmail.com>
2023-02-03 16:23:03 +0800

    Patched dwm-namedscratchpad (Joshua O'Connor)

--- config.def.h
+++ config.def.h
@@ -26,9 +26,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class            instance    title       tags mask     isfloating   monitor    scratch key */
+	{ "Gimp",           NULL,       NULL,       0,            1,           -1,        0  },
+	{ "firefox",        NULL,       NULL,       1 << 8,       0,           -1,        0  },
+    { "Alacritty",      NULL,       "Termpad",  0,            1,           -1,       't' },
+	{ "Alacritty",      NULL,       "Ranger",   0,            1,           -1,       'r' },
 };
 
 /* layout(s) */
@@ -45,7 +47,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -58,13 +60,21 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+
+static const char *scratchpads[][9] = 
+{
+    {"t", "alacritty", "-t", "Termpad", NULL },
+    {"r", "alacritty", "-t", "Ranger", "-e", "ranger", NULL },
+};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+    { MODKEY,                       XK_grave,  togglescratch,  {.v = &scratchpads[0] } },
+    { MODKEY,                       XK_r,      togglescratch,  {.v = &scratchpads[1] } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
