519ee28f685b55070320ca3326fb14b68fd741fd
IVRYN <aizofmulia@gmail.com>
2023-02-04 21:21:07 +0800

    Added additional rules for steam

--- config.def.h
+++ config.def.h
@@ -25,7 +25,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "term", "www", "dev", "play", "virt", "voip", "7", "8", "9" };
+static const char *tags[] = { "term", "www", "dev", "play", "virt", "voip", "recon", "8", "9" };
 static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const int momentaryalttags = 1; /* 1 means alttags will show only when key is held down*/
 
