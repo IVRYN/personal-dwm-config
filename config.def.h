3bc67a7524db01e6aca6b50bda0ef91f8250d5c2
IVRYN <aizofmulia@gmail.com>
2023-02-05 00:26:29 +0800

    Fixed dwm-tatami crashing bug, when manually activated instead of using spacebar

--- config.def.h
+++ config.def.h
@@ -63,7 +63,6 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
-#include "tatami.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
